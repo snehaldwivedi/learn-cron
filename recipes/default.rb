a282753cc546d1982577b1c82aa212220ec37195
snehaldwivedi <sdwivedi@msystechnologies.com>
2020-05-08 10:41:41 +0000

    To display current time in new file at home path

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+cron 'cron_disply_current_time' do
+  minute '*/15'
+  command 'NOW=$(date +"%T") && sudo echo "Current time:"  $NOW > ~/time.txt'
+  action :create
+end
